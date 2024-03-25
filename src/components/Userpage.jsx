4c160a8e7f3a8c6ca722a3a454517006364a0d64
krishna-200 <20mh1a4242@acoe.edu.in>
2024-03-25 13:49:34 +0530

    changes in user page while uploding image

--- src/components/Userpage.jsx
+++ src/components/Userpage.jsx
@@ -16,10 +16,13 @@ const Userpage = () => {
   const param = useParams().id;
   const [fullname, setFullname] = useState("");
   const [mail, setMail] = useState("");
-  const [file, setFile] = useState("");
+  const [file, setFile] = useState(null);
   const [year, setYear] = useState("");
   const [results, setResutls] = useState([]);
   const [score, setScore] = useState([]);
+  const [filePath, setFilePath] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+  const [showButton, setShowButton] = useState(false);
 
   async function getUserData() {
     try {
@@ -71,6 +74,42 @@ const Userpage = () => {
     getUserData();
   }, []);
 
+  function handleFileChange(ev) {
+    setShowButton(true);
+    setFile(ev.target.files[0]);
+
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      setImageUrl(e.target.result);
+    };
+    reader.readAsDataURL(ev.target.files[0]);
+  }
+
+  async function handleFileUpload() {
+    const formData = new FormData();
+    formData.append("file", file);
+    setShowButton(false);
+    const response = await axios.post("/imageUpload", formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+      params: { id: param },
+    });
+  }
+
+  async function getImage() {
+    const response = await axios.get("/getImage", { params: { id: param } });
+    if (response.data.length > 0) {
+      setFilePath(response.data[0].file);
+      console.log(response.data[0].file);
+    }
+    setShowButton(false);
+  }
+
+  useEffect(() => {
+    getImage();
+  }, []);
+
   return (
     <div className={css.container}>
       <div className={css.homeContainer}>
@@ -97,18 +136,30 @@ const Userpage = () => {
         </div>
         <div className={css.profile}>
           <div className={css.avatar}>
-            <label htmlFor="file-upload">
-              <img src={avatar} alt="" />
-            </label>
-            <input type="file" id="file-upload" label="Image" />
+            {filePath ? (
+              <img
+                src={`https://examniationportal-backend.onrender.com/images/${filePath}`}
+                alt="avatar"
+              />
+            ) : imageUrl ? (
+              <img src={imageUrl} alt="Uploaded" />
+            ) : (
+              <label htmlFor="file-upload">
+                <img src={avatar} alt="" />
+              </label>
+            )}
+
+            <input
+              type="file"
+              id="file-upload"
+              label="Image"
+              onChange={handleFileChange}
+            />
+            {showButton ? <button onClick={handleFileUpload}>Save</button> : ""}
             <h2>{fullname}</h2>
           </div>
           <div className={css.line}> </div>
           <div className={css.info}>
-            <span>
-              <LiaBirthdayCakeSolid />
-              <p>03-11-2001</p>
-            </span>
             <span>
               <IoSchoolSharp />
               <p>{year}</p>
